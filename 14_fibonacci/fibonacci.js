3aebcaca947f2a281a71aa09f6112194bdda8297
Nathan <146783884+Corells@users.noreply.github.com>
2025-09-01 18:37:26 -0400

    complete 14_fibonacci

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    if (num < 0)
+        return "OOPS"
+    if (num == 0)
+        return 0
 
+    let first = 1
+    let second = 0
+
+    for (let i = 2; i <= num; i++) {
+        let cur = first + second
+        second = first
+        first = cur
+    }
+
+    return first
 };
 
 // Do not edit below this line
